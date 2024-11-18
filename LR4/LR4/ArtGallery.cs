9eaf7e467d5d2129d1117aba78d374393f816cd8
ShutovaTE <pamada2004@bk.ru>
2024-11-18 22:06:16 +0300

    Поставьте 10, я очень стараюсь

---
+++ LR4/LR4/ArtGallery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LR4;
+
+namespace LR4
+{
+    public class ArtGallery
+    {
+        private List<string> artworks = new List<string>();
+
+        public int ArtworkCount => artworks.Count;
+
+        public void AddArtwork(string artwork)
+        {
+            if (!artworks.Contains(artwork))
+            {
+                artworks.Add(artwork);
+            }
+        }
+
+        public void RemoveArtwork(string artwork)
+        {
+            artworks.Remove(artwork);
+        }
+
+        public bool FindArtwork(string artwork)
+        {
+            return artworks.Contains(artwork);
+        }
+
+    }
+}
---
+++ LR4/LR4Tests/ArtGalleryTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using LR4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LR4Tests;
+
+namespace LR4Tests 
+{ 
+    [TestFixture]
+    public class ArtGalleryTests
+    {
+        [Test]
+        public void AddArtwork_ShouldIncreaseArtworkCount()
+        {
+            var gallery = new ArtGallery();
+            gallery.AddArtwork("Mona Lisa");
+
+            Assert.AreEqual(1, gallery.ArtworkCount);
+        }
+
+        [Test]
+        public void AddArtwork_ShouldNotAllowDuplicateArtworks()
+        {
+            var gallery = new ArtGallery();
+            gallery.AddArtwork("Mona Lisa");
+            gallery.AddArtwork("Mona Lisa");
+
+            Assert.AreEqual(1, gallery.ArtworkCount);
+        }
+
+        [Test]
+        public void RemoveArtwork_ShouldDecreaseArtworkCount()
+        {
+            var gallery = new ArtGallery();
+            gallery.AddArtwork("Mona Lisa");
+            gallery.RemoveArtwork("Mona Lisa");
+
+            Assert.AreEqual(0, gallery.ArtworkCount);
+        }
+
+        [Test]
+        public void FindArtwork_ShouldReturnTrueIfExists()
+        {
+            var gallery = new ArtGallery();
+            gallery.AddArtwork("Mona Lisa");
+
+            Assert.IsTrue(gallery.FindArtwork("Mona Lisa"));
+        }
+
+        [Test]
+        public void ArtworkCount_ShouldBeZeroWhenGalleryIsEmpty()
+        {
+            var gallery = new ArtGallery();
+
+            Assert.AreEqual(0, gallery.ArtworkCount);
+        }
+
+    }
+}
